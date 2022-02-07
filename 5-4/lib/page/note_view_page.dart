7545132f222230a699c123efba7560554de72345
Taeho Kim <jyte82@gmail.com>
2022-02-08 00:40:17 +0900

    [5-4] Migrate to null safety

--- 5-4/lib/page/note_view_page.dart
+++ 5-4/lib/page/note_view_page.dart
@@ -14,7 +14,7 @@ class NoteViewPage extends StatefulWidget {
 class _NoteViewPageState extends State<NoteViewPage> {
   @override
   Widget build(BuildContext context) {
-    NotePageArgs args = ModalRoute.of(context).settings.arguments;
+    NotePageArgs args = ModalRoute.of(context)!.settings.arguments! as NotePageArgs;
     Note note = args.note;
 
     return Scaffold(
