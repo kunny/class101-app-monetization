204207613eccfbee424a5cd389958a76c454336b
Taeho Kim <jyte82@gmail.com>
2022-06-09 09:01:00 +0900

    Update 7-6

--- 7-6/lib/page/note_list_page.dart
+++ 7-6/lib/page/note_list_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sticky_notes/data/note.dart';
 import 'package:sticky_notes/page/note_edit_page.dart';
-import 'package:sticky_notes/page/note_page_args.dart';
 import 'package:sticky_notes/page/note_view_page.dart';
 import 'package:sticky_notes/providers.dart';
 
@@ -21,31 +20,28 @@ class _NoteListPageState extends State<NoteListPage> {
       ),
       body: FutureBuilder<List<Note>>(
         future: noteManager().listNotes(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
             return Center(
               child: CircularProgressIndicator(),
             );
           }
 
-          if (snapshot.hasData) {
-            List<Note> notes = snapshot.data!;
-
-            return GridView.builder(
-              padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                return _buildCard(notes[index]);
-              },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1,
-              ),
+          if (snap.hasError) {
+            return Center(
+              child: Text('오류가 발생했습니다.'),
             );
           }
 
-          return Center(
-            child: Text('오류가 발생했습니다.'),
+          final notes = snap.requireData;
+          return GridView.builder(
+            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+            itemCount: notes.length,
+            itemBuilder: (context, index) => _buildCard(notes[index]),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1,
+            ),
           );
         },
       ),
@@ -67,7 +63,7 @@ class _NoteListPageState extends State<NoteListPage> {
         Navigator.pushNamed(
           context,
           NoteViewPage.routeName,
-          arguments: NotePageArgs(note),
+          arguments: note.id,
         ).then((value) {
           setState(() {});
         });
